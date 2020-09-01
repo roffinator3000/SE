31d5be62d4db71ca84fba43813fc5cb01b8398cf
roffinator3000 <47532386+roffinator3000@users.noreply.github.com>
2020-09-01 18:37:52 +0200

    added contollerIF and ViewIF

---
+++ src/ControllerIF.java
@@ -0,0 +1,14 @@
+/*
+	Author:			R. Kuhn
+	Coauthor:
+	Refactoring:
+	Date:			01.09.2020
+	Time:			15:15
+	Time spent:		0.25 h
+*/
+
+public interface ControllerIF{
+	public void addView(ViewIF newView);
+	public void removeView(ViewIF oldView);
+	public void updater();
+}
---
+++ src/ViewIF.java
@@ -0,0 +1,15 @@
+/*
+	Author:			R. Kuhn
+	Coauthor:		
+	Refactoring:	
+	Date:			01.09.2020
+	Time:			16:28
+	Time spent:		0.25 h
+*/
+
+public interface ViewIF{
+	
+	public void update();
+	public void setController(ControllerIF ctrl);
+	public void setParkhaus(ParkhausIF ph);
+}
