cd3c00a67283d3cfd6410043156823fbd7f3f03c
ld <test@test.de>
2020-09-04 12:18:39 +0200

    Demo 1.0

---
+++ src/Auto.java
@@ -0,0 +1,16 @@
+/*
+  Author:      L.Daskin
+  Coauthor:    
+  Refactoring:  
+  Date:      04.09.20
+  Time:      11:51
+  Time spent:    0.0 h  
+*/
+
+public class Auto {
+    private int id;
+    private int kosten;
+
+
+
+}
---
+++ src/AutoIF.java
@@ -0,0 +1,14 @@
+/*
+  Author:      L.Daskin
+  Coauthor:    
+  Refactoring:  
+  Date:      04.09.20
+  Time:      11:34
+  Time spent:    0.0 h  
+*/
+
+public interface AutoIF {
+
+
+
+}
