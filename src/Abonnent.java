69aa9387a787efe310dca260c678b49c08fcc2ee
ld <test@test.de>
2020-09-04 17:31:09 +0200

    -Interface Auto Fertig -Autoklasse auch Fertig(Strategy pattern für verschiedene Preisberechnung auch Fertig) -Testklasse noch nicht Fertig

---
+++ src/Abonnent.java
@@ -0,0 +1,15 @@
+/*
+  Author:      L.Daskin
+  Coauthor:    
+  Refactoring:  
+  Date:      04.09.20
+  Time:      16:21
+  Time spent:    0.0 h  
+*/
+
+public class Abonnent implements Kunde{
+    @Override
+    public int preisberechnen(int zeit) {
+        return zeit*2;
+    }
+}
---
+++ src/Kunde.java
@@ -0,0 +1,14 @@
+/*
+  Author:      L.Daskin
+  Coauthor:    
+  Refactoring:  
+  Date:      04.09.20
+  Time:      16:18
+  Time spent:    0.02 h
+*/
+
+public interface Kunde {
+
+     int preisberechnen(int zeit);
+
+}
