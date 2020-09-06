556a306f55e215619dc14be08b2df35602b820c2
roffinator3000 <47532386+roffinator3000@users.noreply.github.com>
2020-09-06 22:41:10 +0200

    implemented ParkhausServlet as Controller

--- src/ParkhausServlet.java
+++ src/ParkhausServlet.java
@@ -4,7 +4,7 @@
 	Refactoring:
 	Date:			31.08.2020
 	Time:			15:33
-	Time spent:		0.0h
+	Time spent:		0.0h	5 min
 */
 
 import javax.servlet.ServletException;
@@ -13,14 +13,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet("/Parkhaus")
-public class ParkhausServlet extends HttpServlet{
+public class ParkhausServlet extends HttpServlet implements ControllerIF{
+	List<ViewIF> views = new ArrayList<>();
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 	
 	}
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 	
+	}
+	
+	@Override
+	public void addView(ViewIF newView){
+		views.add(newView);
+	}
+	
+	@Override
+	public void removeView(ViewIF oldView){
+		views.remove(oldView);
+	}
+	
+	@Override
+	public void updater(){
+		for (ViewIF e: views)
+			e.update();
 	}
 }
