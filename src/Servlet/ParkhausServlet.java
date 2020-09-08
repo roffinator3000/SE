c05f3035ea0709ce2730e71c64b4b8193d462375
roffinator3000 <47532386+roffinator3000@users.noreply.github.com>
2020-09-08 12:32:03 +0200

    prepared ParkhausServlet for working with Parkhaus

--- src/Servlet/ParkhausServlet.java
+++ src/Servlet/ParkhausServlet.java
@@ -4,7 +4,7 @@
 	Refactoring:
 	Date:			31.08.2020
 	Time:			15:33
-	Time spent:		0.3 h
+	Time spent:		0.5 h
 */
 package Servlet;
 
@@ -15,7 +15,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 @WebServlet("/Parkhaus")
@@ -23,7 +26,19 @@ public class ParkhausServlet extends HttpServlet implements ControllerIF{
 	private static ArrayList<ViewIF> views = new ArrayList<>();
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-	
+		String[] body = getBody(request).split(",");
+		System.out.println(body);
+		
+		switch (body[0]){
+			case "enter":
+				//todo an parkhaus übergeben
+				updater();
+				break;
+			case "leave":
+				//todo an parkhaus übergeben
+				updater();
+				break;
+		}
 	}
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
@@ -53,5 +68,29 @@ public class ParkhausServlet extends HttpServlet implements ControllerIF{
 	public void updater(){
 		for (ViewIF e : views)
 			e.update();
+	}
+	
+	
+	private static String getBody(HttpServletRequest request) throws IOException{    //copied from M. Kaul
+		StringBuilder stringBuilder = new StringBuilder();
+		BufferedReader bufferedReader = null;
+		try{
+			InputStream inputStream = request.getInputStream();
+			if (inputStream != null){
+				bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+				char[] charBuffer = new char[128];
+				int bytesRead = -1;
+				while ((bytesRead = bufferedReader.read(charBuffer)) > 0){
+					stringBuilder.append(charBuffer, 0, bytesRead);
+				}
+			} else{
+				stringBuilder.append("");
+			}
+		} finally{
+			if (bufferedReader != null){
+				bufferedReader.close();
+			}
+		}
+		return stringBuilder.toString();
 	}
 }
