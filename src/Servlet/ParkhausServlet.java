c935bcde96515f3c8bd215d68d491c5fb8bc28b5
roffinator3000 <47532386+roffinator3000@users.noreply.github.com>
2020-09-08 12:29:27 +0200

    moved ParkhausServlet into  package and added futher functions usable vor MVC

--- src/Servlet/ParkhausServlet.java
+++ src/Servlet/ParkhausServlet.java
@@ -1,11 +1,14 @@
 /*
-	Author:			R.Kuhn
+	Author:			R. Kuhn
 	Coauthor:
 	Refactoring:
 	Date:			31.08.2020
 	Time:			15:33
-	Time spent:		0.0h	5 min
+	Time spent:		0.3 h
 */
+package Servlet;
+
+import views.ViewIF;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,11 +17,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 @WebServlet("/Parkhaus")
 public class ParkhausServlet extends HttpServlet implements ControllerIF{
-	List<ViewIF> views = new ArrayList<>();
+	private static ArrayList<ViewIF> views = new ArrayList<>();
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 	
@@ -27,6 +29,15 @@ public class ParkhausServlet extends HttpServlet implements ControllerIF{
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 	
 	}
+	
+	public static boolean hasNoViews(){
+		return views.isEmpty();
+	}
+	
+	public static ArrayList<ViewIF> getViews(){
+		return views;
+	}
+	
 	
 	@Override
 	public void addView(ViewIF newView){
@@ -40,7 +51,7 @@ public class ParkhausServlet extends HttpServlet implements ControllerIF{
 	
 	@Override
 	public void updater(){
-		for (ViewIF e: views)
+		for (ViewIF e : views)
 			e.update();
 	}
 }
